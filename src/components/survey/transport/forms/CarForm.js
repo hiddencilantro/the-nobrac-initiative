04e06989f094fa5c57b0c410f469ae50cfa2161d
hiddencilantro <stevekim1109@gmail.com>
2022-02-25 14:32:49 -0500

    car form display logic & navigate hook (commented out)

--- src/components/survey/transport/forms/CarForm.js
+++ src/components/survey/transport/forms/CarForm.js
@@ -1,13 +1,28 @@
+// import { useNavigate } from 'react-router-dom';
+
 function CarForm({display, car, setCar}) {
-    return display === "true" ? (
-        <form>
-            <label htmlFor="car">How many miles do you drive per year?</label>
-            <br />
-            <input type="text" id="car" value={car} onChange={e => isNaN(e.target.value) ? e.target.value="" : setCar(pS => ({...pS, parameters: {...pS.parameters, distance: e.target.value}}))} /> miles
-            <br />
-            <input type="submit" value="Next"/>
-        </form>
-    ) : null
+    // const navigate = useNavigate();
+
+    const handleNext = e => {
+        e.preventDefault();
+        // navigate('/survey/transport/bus')
+    };
+
+    if (display === "true") {
+        return (
+            <form onSubmit={handleNext}>
+                <label htmlFor="car">How many miles do you drive per year?</label>
+                <br />
+                <input type="text" id="car" value={car} onChange={e => isNaN(e.target.value) ? e.target.value="" : setCar(pS => ({...pS, parameters: {...pS.parameters, distance: e.target.value}}))} /> miles
+                <br />
+                <input type="submit" value="Next"/>
+            </form>
+        );
+    } else if (display === "false") {
+        return <button onClick={handleNext}>Next</button>;
+    } else {
+        return null;
+    };
 }
 
 export default CarForm;
