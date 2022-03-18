9c989b5a4ed22d3d5737bd684c6f5decd235aa47
hiddencilantro <stevekim1109@gmail.com>
2022-03-17 22:27:33 -0400

    signup and login modals moved to App component, new routes (nested) for saved user footprints

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Signup from './components/Signup';
+import Login from './components/Login';
 import Landing from './components/Landing';
 import Mission from './components/Mission';
 import Survey from './components/Survey';
+import Footprints from './components/Footprints';
+import EmissionData from './components/EmissionData';
 import NotFound from './components/NotFound';
 
 function App() {
+  const [signup, setSignup] = useState(false);
+  const [login, setLogin] = useState(false);
+
   return (
     <div className="App">
       <NavBar />
+      <Signup signup={signup} setSignup={setSignup} setLogin={setLogin} />
+      <Login login={login} setLogin={setLogin} setSignup={setSignup} />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/mission" element={<Mission />} />
-        <Route path="survey/*" element={<Survey />} />
+        <Route path="mission" element={<Mission />} />
+        <Route path="survey/*" element={<Survey setSignup={setSignup} />} />
+        <Route path="footprints" element={<Footprints setLogin={setLogin} />} >
+          <Route path=":emissionId" element={<EmissionData />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
