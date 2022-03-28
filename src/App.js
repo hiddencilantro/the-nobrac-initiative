4ef2cfc3e403ee48dfa77c98e01101134a28e63d
hiddencilantro <stevekim1109@gmail.com>
2022-03-28 16:07:56 -0400

    higher order component for window.scrollTo(0, 0)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import ScrollToTop from './components/ScrollToTop';
 import Landing from './components/Landing';
 import Problem from './components/Problem';
 import Quiz from './components/Quiz';
@@ -20,15 +21,17 @@ function App() {
       <Signup signup={signup} setSignup={setSignup} setLogin={setLogin} />
       <Login login={login} setLogin={setLogin} setSignup={setSignup} />
       <NavBar setLogin={setLogin} />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="problem" element={<Problem />} />
-        <Route path="quiz/*" element={<Quiz setLogin={setLogin} setCreateSuccess={setCreateSuccess} />} />
-        <Route path="footprints" element={<Footprints setLogin={setLogin} 
-            createSuccess={createSuccess} setCreateSuccess={setCreateSuccess} />} >
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="problem" element={<Problem />} />
+          <Route path="quiz/*" element={<Quiz setLogin={setLogin} setCreateSuccess={setCreateSuccess} />} />
+          <Route path="footprints" element={<Footprints setLogin={setLogin} 
+              createSuccess={createSuccess} setCreateSuccess={setCreateSuccess} />} >
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ScrollToTop>
     </div>
   );
 }
